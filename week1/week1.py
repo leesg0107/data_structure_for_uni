b5a7fec478b950c72fa2e6ea4db048f638301a17
leesg17 <leesg0107@gmail.com>
2024-03-12 16:39:26 +0900

    week1 commit

--- week1/week1.py
+++ week1/week1.py
@@ -1,5 +1,29 @@
 #recursive function
 #실습 환경은 colab or vscode recommend
+def evaluate_postfix(tokens):
+    if not tokens:
+        return None
+    token=tokens.pop()
+    if token.isnumeric():
+        return float(token)
+    else:
+        operation2=evaluate_postfix(tokens)
+        operation1=evaluate_postfix(tokens)
+        if token=="+":
+            return operation1+operation2
+        elif token=="-":
+            return operation1-operation2
+        elif token=="*":
+            return operation1*operation2
+        elif token=="/":
+            return operation1/operation2
+        elif token=="^":
+            return operation1**operation2
+postfix="2 3 4 + *"
+tokens=postfix.split()
+result=evaluate_postfix(tokens.copy())
+print(result)
+
 
 #연습문제 1
 def sen(n):
@@ -30,9 +54,15 @@ def pow(x,n):
 print(pow(3,3))
 
 #연습문제 4 (find max)
-num=[2,8,10,7,6,1,3,9]
+num=[2,8,10,7,6,1,31,9]
+n=len(num)
 def find_max(num,n):
     if n==1:
         return num[0]
     else:
-        return
+        max=find_max(num,n-1)
+        if max>num[n-1]:
+            return max
+        else:
+            return num[n-1]
+print(find_max(num,n))
