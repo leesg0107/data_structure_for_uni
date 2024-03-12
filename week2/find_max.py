97724b8d4ba38b2066155dc35eb8afb3f06b3ee6
leesg17 <leesg0107@gmail.com>
2024-03-12 17:18:28 +0900

    complete version

--- week2/find_max.py
+++ week2/find_max.py
@@ -1,16 +1,27 @@
 #if user enter numbers, return them
 
-def find_max(num,n):
+def find_max_recursive(num,n):
     if n==1:
         return num[0]
     else:
-        if find_max(num,n-1)>num[n-1]:
-            return find_max(num,n-1)
+        if find_max_recursive(num,n-1)>num[n-1]:
+            return find_max_recursive(num,n-1)
         else:
             return num[n-1]
 
+def find_max_for(num,n):
+    max=num[0]
+    for i in range(1,n):
+        if max<num[i]:
+            max=num[i]
+        else:
+            max=max
+    return max
+
 num = []
-for i in range(0, 3):
-    number = int(input("Enter a %d th number: " % (i + 1)))  # 입력값을 정수로 변환
+for i in range(0, 10):
+    number = float(input("Enter a %d th number: " % (i + 1)))
     num.append(number)
-print(find_max(num, len(num)))
+
+print(find_max_recursive(num, len(num)))
+print(find_max_for(num, len(num)))
