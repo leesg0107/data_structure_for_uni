c868d215d19e6893c01bc9ce8273deeebfedbc45
leesg17 <leesg0107@gmail.com>
2024-03-18 22:22:46 +0900

    firstassignment

--- week3/lru_sim/lru_sim.py
+++ week3/lru_sim/lru_sim.py
@@ -2,23 +2,23 @@
 
 class CacheSimulator:
     def __init__(self, cache_slots):
-        self.cache_slots = cache_slots
+        self.cache_slots=[]
         self.cache_hit = 0
         self.tot_cnt = 1
+        self.cache_size=cache_slots
 
-    def do_sim(self, page):
-        self.tot_cnt += 1
+    def do_sim(self,page):
+        self.tot_cnt+=1
         if page in self.cache_slots:
-            self.cache_hit += 1
+            self.cache_hit+=1
         else:
-            if len(self.cache_slots) < self.cache_slots:
+            if len(self.cache_slots)<self.cache_size:
                 self.cache_slots.append(page)
             else:
                 self.cache_slots.pop(0)
                 self.cache_slots.append(page)
-
     def print_stats(self):
-        print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
+        print("cache_slot = ", self.cache_size, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
 
 
 if __name__ == "__main__":
